b2f39764bf24a31412ee8526e640a1ab34a04ffd
hpraveen <hariharanster@gmail.com>
2019-06-16 19:02:58 -0400

    Created all required folder

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//Create an array that consists of a set of words 
+var wordlist=["trait","hover","blame","basin","raise","title","shell","tight","chalk","study","blade",
+"issue","guard","spill","tiger"];
+
+//Pick a random word from wordlist
+
+var rnumber=wordlist[Math.floor(Math.random()*wordlist.length)];
+
+console.log(rnumber);

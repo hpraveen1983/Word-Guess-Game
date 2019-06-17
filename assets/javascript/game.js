a571b55e6a0837dce4d38922ce3ea58b22d38f71
hpraveen <hariharanster@gmail.com>
2019-06-17 13:20:55 -0400

    Created pseudo code for this game

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+//Create an array of words from which we can randomly pick a word for the user to guess
+
+
+//We should be able to randomly pick a word from the array
+
+
+//We should be able to display the number of characters in the word to the user
+
+
+//How many characters were guessed and the remaining characters should be stored
+
+
+//While loop to keep guessing the word
+
+
+//User's guess
+
+
+
+//Scenarios involved while guessing
+Scenario1
+Scenario2
+Scenario3
+
+
+//if the character is part of the guess word then update display array
+
+
+

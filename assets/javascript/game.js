511be800a389596ae793801dd0c1f5017e64d9a3
hpraveen <hariharanster@gmail.com>
2019-06-18 07:16:21 -0400

    Captured user guess and keeping a count of it. Remaining characters is also calculated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,73 @@
 //Create an array of words from which we can randomly pick a word for the user to guess
-var wordlist = ["trait", "hover", "blame", "basin", "raise", "title", "shell", "tight", "chalk", "study", "blade",
-"issue", "guard", "spill", "tiger"];
+var wordlist = ["trait"];
 
 //We should be able to randomly pick a word from the array
 
-var rnumber = wordlist[Math.floor(Math.random() * wordlist.length)]; 
+var rnumber = wordlist[Math.floor(Math.random() * wordlist.length)];
+
+//number of guesses remaining 
+
+//var guessesLeft;
+
 
 //We should be able to display the number of characters in the word to the user
 
-var dArr = [];
+var dispArr = [];
 for (var i = 0; i < rnumber.length; i++) {
-dArr[i] = "_";
+    dispArr[i] = "_";
+    //alert("Your progress: "+dispArr[i])
 }
 
 //How many characters were guessed and the remaining characters should be stored
 
-var ralpha=rnumber.length;
+var ralpha = rnumber.length;
+
+var guessCount=0;
+//var rGuess=8;
+
+while (ralpha > 0) {
+    alert(dispArr.join(" "));
 
 
-//While loop to keep guessing the word
+    //User's guess
+
+    var uGuess = prompt("What is your guess?");
+    guessCount=guessCount+1;
+/*     rGuess=rGuess-guessCount;
+        alert("You have "+rGuess+" guesses remaining"); */
 
 
-//User's guess
+    //Scenarios involved where remaining alphabets is greater than zero
 
+    if (uGuess == null) { break;}
+    else if (uGuess.length !== 1) {
+        alert("Enter one alphabet please!");
+    }  else if(guessCount>7) {
+        document.write("You lost the game");
+        break;
+    }     
+    else {
+        for (var x = 0; x < rnumber.length; x++) {
+            if (rnumber[x] === uGuess) {
+                dispArr[x] = uGuess;
+                    ralpha--;
+/*                     rGuess=rGuess-guessCount;
+                    alert("You have "+rGuess+" guesses remaining");    */         
+/*                     console.log(ralpha);
+                guessesLeft=(8-ralpha);
+                alert("You have "+ guessesLeft+" more characters to guess") */
+               
+            } 
+        }
+    }
 
+}  /* else if(guessCount>=8) {
+    alert("You lost the game")
+}  */
+//alert(dispArr.join(" "));
 
-//Scenarios involved while guessing
-Scenario1
-Scenario2
-Scenario3
-
-
-//if the character is part of the guess word then update display array
+//document.write("<h1> You Guessed the Word!<h1>");
+//document.write("It took you "+guessCount+ " tries to get the word "+rnumber);
 
 
 

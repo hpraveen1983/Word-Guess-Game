f4c9a275cade7bac4469e281bdf7f7f55c591bb5
hpraveen <hariharanster@gmail.com>
2019-06-18 23:52:46 -0400

    Included wins,loss and guess word in the webpage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 //Create an array of words from which we can randomly pick a word for the user to guess
 var wordlist =["hover","scandal","withdraw","time","tycoon","relief","revenge","drill","carriage","resident"];
 
-
 //We should be able to randomly pick a word from the array
 
 var rWord = wordlist[Math.floor(Math.random() * wordlist.length)];
@@ -23,6 +22,7 @@ for (var i = 0; i < rWord.length; i++) {
 
 var ralpha = rWord.length;
 var wins = 0;
+var loss = 0;
 
 //set guess count to zero
 var guessCount = 0;
@@ -48,8 +48,9 @@ while (ralpha > 0) {
     if (userGuess == null) { break; }
     else if (userGuess.length !== 1) {
         alert("Enter one alphabet please!");
-    } else if (guessCount > 7) {
-        alert("You lost the game");
+    } else if (guessCount > 10) {
+        //loss=alert("You lost the game")
+        loss++;
         break;
     }
     else {
@@ -58,22 +59,20 @@ while (ralpha > 0) {
                 dispArr[x] = userGuess;
                 ralpha--;
                 lArr.push(dispArr[x]);
-            } 
-
+            }
             }
             if (lArr.length === rWord.length) {
                 wins++;
-                //alert(wins);
-
         }
     }
 }
 
 
 
-document.getElementById("guess-log").innerHTML = lArr;
-document.getElementById("remaining-alpha").innerHTML = ralpha;
+document.getElementById("guess-log").innerHTML = guessCount;
+document.getElementById("remaining-alpha").innerHTML = loss;
 document.getElementById("win-log").innerHTML = wins;
+document.getElementById("rword").innerHTML = rWord;
 
 
 
